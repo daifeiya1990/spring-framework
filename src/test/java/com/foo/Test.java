b3dfaaa48d5556b04de1cc53f1fec1fe05381cb1
Administrator <feiya.dai@qgtechnology.com>
2019-05-08 23:08:51 +0800

    补充了注释

---
+++ src/test/java/com/foo/Test.java
@@ -0,0 +1,26 @@
+package com.foo;
+
+import org.springframework.beans.factory.support.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.Lifecycle;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.AliasRegistry;
+import org.springframework.core.env.*;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.StandardServletEnvironment;
+
+/**
+ * @author daify
+ * @date 2019-03-18
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		Lifecycle
+	}
+}
